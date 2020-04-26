c5ce951c8a3b1cfacee0d624818b3a45d3b6c905
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-04-26 16:03:11 -0400

    working on outPut

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
+}
+
+var possibleSelection = []
+var includeLowerCase;
+var includeUpperCase;
+var includeNumber;
+var includeSpecial;
 
 var lowerCasedCharacters = [
   'a',
@@ -84,7 +99,6 @@ var specialCharacters = [
   '.'
 ];
 
-var possibleSelection = []
 
 function generatePassword () {
   var tellUsWhatToInclude = alert("Please answer the following questions in order to generate your pasword!")
@@ -92,55 +106,43 @@ function generatePassword () {
   var includeUpperCase = prompt("Do you want to include upper case letters?")
   var includeNumber = prompt("Do you want to include numbers?")
   var includeSpecial = prompt("Do you want to include special characters?")
-  var outPut = ""
+ 
 
  
   if (includeLowerCase == "yes" ) {
     console.log("User wants to include lower case")
-    possibleSelection.push(lowerCasedCharacters)
+    possibleSelection = possibleSelection.push(lowerCasedCharacters)
     console.log(possibleSelection)
   }
   if (includeUpperCase == "yes") {
     console.log ("User wants to include upper case")
-    possibleSelection.push(upperCasedCharacters)
+    possibleSelection = possibleSelection.push(upperCasedCharacters)
     console.log(possibleSelection)
   }
   if (includeNumber == "yes") {
     console.log ("User wants to include numbers")
-    possibleSelection.push(numericCharacters)
+    newPass = possibleSelection.push(numericCharacters)
     console.log(possibleSelection)
   }
   if (includeSpecial == "yes") {
     console.log ("User wants to include special characters")
-    possibleSelection.push(specialCharacters)
-    console.log(possibleSelection)
-
+    possibleSelection = possibleSelection.push(specialCharacters)
+    console.log(newPass)
+   
   }
  
   var passwordLength = parseInt(prompt("How long do you want your password to be? (It must be between 8 and 128 characters"))
   console.log(passwordLength) 
 
+  var outPut = ""
+
   for (var i = 0; i < passwordLength; i++){
-    outPut = Math.floor(Math.random() * all.passwordLength);
-    password += all.passwordLength(outPut, outPut + 1);
+    outPut = outPut + possibleSelection[Math.floor(Math.random() * possibleSelection.length)];
+    console.log(outPut + possibleSelection[Math.floor(Math.random() * possibleSelection.length)
   }
 
 return outPut
 }
 
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
-generateBtn.addEventListener("click", writePassword);
-
-for(var i = 0; i < 12; i++){
-  console.log(numericCharacters[i])
-}
 
